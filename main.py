14138141c876515b5f2bf343f7439d19ee2cf14a
czinczewski <czinczewski@gmail.com>
2017-04-04 00:18:34 +0200

    Początki Bazy Danych
    Nauka tworzenia baz danych w Pythonie z wykorzystaniem 'sqlite3' .

---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3 as sql
+
+print("sqllite3 version: ", sql.version)
+
+# --------------------------------------------------------
+db = sql.connect('./database/database.db')
+coursor = db.cursor()
+
+
+# --------------------------------------------------------
+def create_db():
+    coursor.execute('CREATE TABLE IF NOT EXISTS try(inx REAL, datestamp TEXY, keyword TEXT, value REAL)')
+
+
+# --------------------------------------------------------
+def read_data_from_txt():
+    print("Reading data from .txt file")
+    data = []
+    return data
+
+
+# --------------------------------------------------------
+def data_entry(data):
+    print("Dodaje dane")
+    coursor.execute("INSERT INTO try VALUES(1545564, '2016-01-01', 'Python', 5)")
+    db.commit()
+    coursor.close()
+    db.close()
+
+
+# --------------------------------------------------------
+create_db()
+data = read_data_from_txt()
+data_entry(data)
