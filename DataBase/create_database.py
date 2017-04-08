dde0afcf2f1015181b0a1c1cb7e5efc0687f0f15
czinczewski <czinczewski@gmail.com>
2017-04-09 00:06:40 +0200

    UPDATE databse INSERT
    Dodano przykładowe dane do bazy

--- DataBase/create_database.py
+++ DataBase/create_database.py
@@ -1,8 +1,8 @@
 import sqlite3 as sql
 print("SQLite version: ", sql.version, "\n")
-import datetime     # datestamp
-import random       # real data
 
+db = sql.connect('./database.db')
+coursor = db.cursor()
 
 # --------------------------------------------------------
 def create_db():
@@ -12,7 +12,7 @@ def create_db():
                         name TEXT,
                         surname TEXT,
                         id_number TEXT,
-                        card_number REAL)''')
+                        card_number TEXT)''')
     print("Table CLIENT created successfully")
 
     coursor.execute('''CREATE TABLE IF NOT EXISTS city
@@ -23,13 +23,13 @@ def create_db():
 
     coursor.execute('''CREATE TABLE IF NOT EXISTS flat
                         (id_flat INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                        availabity INTEGER NOT NULL,
+                        availability TEXT NOT NULL,
                         start_date TEXT,
                         end_date TEXT,
                         price REAL,
                         number_of_rooms INTEGER NOT NULL,
                         amount_of_people INTEGER NOT NULL,
-                        animalas TEXT,
+                        animals TEXT,
                         childs TEXT,
                         parking_space TEXT,
                         id_city INTEGER REFERENCES city(id_city))''')
@@ -42,15 +42,61 @@ def create_db():
                         start_date TEXT,
                         end_date TEXT)''')
     print("Table RENT created successfully")
+    print("\n")
+    db.commit()
+
+# --------------------------------------------------------
+def insert_data():
+    clients = [('andrzej123',   'Andrzej',  'Dobrowolski',  'AAA 305462', '1234123412341234'),
+               ('slodkajola',   'Joanna',   'Plichta',      'ABC 658942', '1235123512351235'),
+               ('kubon',        'Jakub',    'Turewicz',     'FDA 652374', '4567456745674895'),
+               ('rudy',         'Janusz',   'Cebula',       'CEB 154621', '5641523647895245'),
+               ('karolix',      'Karol',    'Sobisz',       'GHB 564895', '5123486432165494')]
+    coursor.executemany('''INSERT INTO client(nickname, name, surname, id_number, card_number)
+                            VALUES (?, ?, ?, ?, ?)''', (clients))
+
+    city = [('Gdansk', '80-404'),
+            ('Sopot', '81-701'),
+            ('Gdynia', '12-213'),
+            ('Tokyo', '132-4315')]
+    coursor.executemany('''INSERT INTO city(name, zip_code) VALUES (?, ?)''', (city))
+
+    flats = [('YES', '2017-03-21', '2017-08-20',     120,    2,  4,  'YES',  'YES',  'YES',  1),
+            ('YES', '2017-03-21', '2017-08-20',     100,    2,  3,  'YES',  'YES',  'NO',   2),
+            ('YES', '2017-03-21', '2017-08-20',      30,    1,  2,  'NO',   'NO',   'NO',   1),
+            ('NO',  '2017-03-21', '2017-08-20',      45,    1,  2,  'YES',  'YES',  'NO',   0),
+            ('YES', '2017-03-21', '2017-08-20',      36,    1,  2,  'NO',   'YES',  'NO',   0),
+            ('YES', '2017-03-21', '2017-08-20',     135,    3,  7,  'YES',  'YES',  'YES',  2),
+            ('NO',  '2017-03-21', '2017-08-20',     500,    6,  8,  'NO',   'NO',   'YES',  3)]
+    coursor.executemany('''INSERT INTO flat(availability, start_date, end_date,
+                            price, number_of_rooms, amount_of_people, animals,
+                            childs, parking_space, id_city)
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (flats))
+
+
+
+    db.commit()
 
 
 # --------------------------------------------------------
-def read_data_from_txt():
-    print("Reading data from .txt file")
-    data = []
-    return data
+def show_data():
+    coursor.execute("SELECT * FROM client")
+    print(" ID_CLIENT |    NICKNAME  |        NAME |              SURNAME |  ID_NUMBER |      CARD_NUMBER |")
+    for row in coursor:
+        print('{:>10}'.format(row[0]), "|", '{:>12}'.format(row[1]), "|", '{:>11}'.format(row[2]), "|", '{:>20}'.format(row[3]), "|", '{:>10}'.format(row[4]), "|", '{:>16}'.format(row[5]), "|")
 
-# # --------------------------------------------------------
+    print("\n")
+
+    coursor.execute("SELECT * FROM city")
+    print(" ID_CITY |       NAME |   ZIP_CODE |")
+    for row in coursor:
+        print('{:>8}'.format(row[0]), "|", '{:>10}'.format(row[1]), "|", '{:>10}'.format(row[2]), "|")
+
+
+    # print(coursor.fetchall())
+
+
+# --------------------------------------------------------
 # def data_entry(data):
 #     print("Dodaje dane")
 #     coursor.execute("INSERT INTO try VALUES(1545564, '2016-01-01', 'Python', 5)")
@@ -58,10 +104,10 @@ def read_data_from_txt():
 #     coursor.close()
 #     db.close()
 # --------------------------------------------------------
-db = sql.connect('./database.db')
-coursor = db.cursor()
 
 create_db()
-data = read_data_from_txt()
+insert_data()
+show_data()
 
+coursor.close()
 db.close()
